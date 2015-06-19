b1040d0d3a7d999e9b383e7da4f35f7ad8178fcb
Theo Costain <theocostain1@gmail.com>
2015-06-19 14:32:30 +0000

    Added search cases for both firstName and lastName

--- registration.php
+++ registration.php
@@ -43,6 +43,8 @@
                                     echo '<h1>Something went really wrong! Please record what happened and what you did before it happened and tell the IT Rep.';
                                 }
                             }
+                        }elseif ($countResult['count']==0){ //Case for no results
+                            echo '<h4>No matching results found.</h4>';
                         }else{      //Case for multiple results.
                             $memberQuery = $db->query('SELECT id, firstName, lastName, cardno FROM '.$members.' WHERE cardno='.$_POST['cardno'].';');
                             echo '<table><tr><th>First Name</th><th>Last Name</th><th>Bod Card Number</th></tr>';
@@ -58,8 +60,8 @@
                     //firstName clash check
                     $countQuery = $db->query('SELECT count(*) as count FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'";');
                     if ($countQuery instanceof SQLite3Result){
-                        $memberResult = $countQuery->fetchArray(SQLITE3_ASSOC);
-                        if ($memberResult['count']==1){  //Case for no clashes
+                        $countResult = $countQuery->fetchArray(SQLITE3_ASSOC);
+                        if ($countResult['count']==1){  //Case for no clashes
                             $memberQuery = $db->query('SELECT id, firstName, lastName FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'";');
                             if ($memberQuery instanceof SQLite3Result){
                                 $memberResult = $memberQuery->fetchArray(SQLITE3_ASSOC);
@@ -69,6 +71,8 @@
                                     echo '<h1>Something went really wrong! Please record what happened and what you did before it happened and tell the IT Rep.';
                                 }
                             }
+                        }elseif ($countResult['count']==0){ //Case for no results
+                            echo '<h4>No matching results found.</h4>';
                         }else{  //Case for multiple results
                             $memberQuery = $db->query('SELECT id, firstName, lastName, cardno FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'";');
                             echo '<table><tr><th>First Name</th><th>Last Name</th><th>Bod Card Number</th></tr>';
@@ -84,8 +88,8 @@
                     //lastName clash check
                     $countQuery = $db->query('SELECT count(*) as count FROM '.$members.' WHERE lastName="'.$_POST['lastName'].'";');
                     if ($countQuery instanceof SQLite3Result){
-                        $memberResult = $countQuery->fetchArray(SQLITE3_ASSOC);
-                        if ($memberResult['count']==1){  //Case for no clashes
+                        $countResult = $countQuery->fetchArray(SQLITE3_ASSOC);
+                        if ($countResult['count']==1){  //Case for no clashes
                             $memberQuery = $db->query('SELECT id, firstName, lastName FROM '.$members.' WHERE lastName="'.$_POST['lastName'].'";');
                             if ($memberQuery instanceof SQLite3Result){
                                 $memberResult = $memberQuery->fetchArray(SQLITE3_ASSOC);
@@ -95,6 +99,8 @@
                                     echo '<h1>Something went really wrong! Please record what happened and what you did before it happened and tell the IT Rep.';
                                 }
                             }
+                        }elseif ($countResult['count']==0){ //Case for no results
+                            echo '<h4>No matching results found.</h4>';
                         }else{  //Case for multiple results
                             $memberQuery = $db->query('SELECT id, firstName, lastName, cardno FROM '.$members.' WHERE lastName="'.$_POST['lastName'].'";');
                             echo '<table><tr><th>First Name</th><th>Last Name</th><th>Bod Card Number</th></tr>';
@@ -107,6 +113,44 @@
                         }
                     }
                     
+                }elseif (($_POST['firstName'] != '') && ($_POST['lastName'] != '')){ //firstName and lastName searches
+                    $andCountQuery = $db->query('SELECT count(*) as count FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'" AND lastName="'.$_POST['lastName'].'";');
+                    $orCountQuery = $db->query('SELECT count(*) as count FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'" OR lastName="'.$_POST['lastName'].'";');
+                    if (($andCountQuery instanceof SQLite3Result) && ($orCountQuery instanceof SQLite3Result)){
+                        $andCountResult = $andCountQuery->fetchArray(SQLITE3_ASSOC);
+                        $orCountResult = $orCountQuery->fetchArray(SQLITE3_ASSOC);
+                        if ($andCountResult['count']==1){//Case where andcount is 1
+                            $memberQuery = $db->query('SELECT id, firstName, lastName FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'" AND lastName="'.$_POST['lastName'].'";');
+                            if ($memberQuery instanceof SQLite3Result){
+                                $memberResult = $memberQuery->fetchArray(SQLITE3_ASSOC);
+                                if ($db->exec('UPDATE '.$members.' SET sessionsAttended = sessionsAttended + 1 WHERE cardno='.$memberResult['id'].';')){
+                                    echo '<h4>'.$memberResult['firstName'].' '.$memberResult['lastName'].' marked as attending.</h4>';
+                                }else{
+                                    echo '<h1>Something went really wrong! Please record what happened and what you did before it happened and tell the IT Rep.';
+                                }
+                            }
+                        }elseif ($andCountResult['count']>1){//Case where andcount >1
+                            $memberQuery = $db->query('SELECT id, firstName, lastName, cardno FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'" AND lastName="'.$_POST['lastName'].'";');
+                            echo '<table><tr><th>First Name</th><th>Last Name</th><th>Bod Card Number</th></tr>';
+                            while ($row = $memberQuery->fetchArray(SQLITE3_ASSOC)){
+                                    echo '<tr><td>'.$row['firstName'].'</td><td>'.$row['lastName'].'</td><td>'.$row['cardno'].'</td>';
+                                    echo '<td><form method="post" action""><input style="visibility:hidden;display:none;" type="text" name="id" value="'.$row['id'].'"/><input type="submit" class="memberbutton" value="Mark Attending"/></form></td>';
+                            }
+                            echo '</table>';
+                            echo "\n";
+                        }elseif ($orCountResult['count']>1){//Case orcount >1
+                            $memberQuery = $db->query('SELECT id, firstName, lastName, cardno FROM '.$members.' WHERE firstName="'.$_POST['firstName'].'" OR lastName="'.$_POST['lastName'].'";');
+                            echo '<table><tr><th>First Name</th><th>Last Name</th><th>Bod Card Number</th></tr>';
+                            while ($row = $memberQuery->fetchArray(SQLITE3_ASSOC)){
+                                    echo '<tr><td>'.$row['firstName'].'</td><td>'.$row['lastName'].'</td><td>'.$row['cardno'].'</td>';
+                                    echo '<td><form method="post" action""><input style="visibility:hidden;display:none;" type="text" name="id" value="'.$row['id'].'"/><input type="submit" class="memberbutton" value="Mark Attending"/></form></td>';
+                            }
+                            echo '</table>';
+                            echo "\n";
+                        }elseif ($orCountResult['count']==0){//Case both 0
+                            echo '<h4>No matching results found.</h4>';
+                        }
+                    }
                 }
             }else{
                 echo "<h5>New Member</h5>";
