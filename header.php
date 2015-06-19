4f02335a792b44fb9c4fe63d681c13b58d01dc41
Theo Costain <theocostain1@gmail.com>
2015-06-19 14:05:45 +0000

    Cleaned up directory

--- header.php
+++ header.php
@@ -6,15 +6,15 @@
     <head> 
         <title>Alts Registration </title>
         <link rel="stylesheet" href="site.css" type="text/css" />
-        <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/images/icons/favicon-16x16.png" sizes="16x16" />
     </head>
     <body bgcolor="#AAAAAA">
         <div class="header">
             <table id='headertable'>
                 <tr>
                     <th id="logo">
-                        <a href="/"><image src="logo.png" alt="Logo"></a>
+                        <a href="/"><image src="/images/logo.png" alt="Logo"></a>
                     </th>
                     <th>
                         <ul id="navlist">
